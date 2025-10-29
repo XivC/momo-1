2b869931be9f7c10afb15ced3b6465a5f253931d
Eldar <ekurmanaliev@inbox.ru>
2025-10-29 13:13:11 +0300

    added load tests

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from better_svd import truncated_svd_power_iteration
 import numpy as np
 
 if __name__ == "__main__":
-    A = Matrix([
-        [4.0, 1.0, 1.0],
-        [1.0, 3.0, 0.0],
-        [1.0, 0.0, 2.0],
-    ])
+    a = np.random.randn(3, 3)
+    print(a)
+    A = Matrix(a.tolist())
 
     vals, _ = qr_algorithm(A)
     print("Eigenvectors:", vals)
@@ -16,11 +14,7 @@ if __name__ == "__main__":
     vals2, vecs = qr_algorithm(A, return_eigenvectors=True)
     print("Eigenvalues (sym):", vals)
     print("Eigenvectors (columns):\n", vecs)
-    asd = np.array([
-        [4.0, 1.0, 1.0],
-        [1.0, 3.0, 0.0],
-        [1.0, 0.0, 2.0],
-    ])
+    asd =  a
     U, S, Vt = truncated_svd_power_iteration(asd, k=3)
     print("--- Итеративный SVD ---")
     print("U:", U)
